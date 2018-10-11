76c639523814e83a960cc575f6771210de334e3a
NimaQu <admin@nimaqu.com>
2018-10-11 22:30:42 +0800

    [A]VUE移动端部署成功，引入fastClick，border 1像素CSS，reset.css与meta name ='viewport'

--- front/Vue/phone-Travel/src/main.js
+++ front/Vue/phone-Travel/src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 解决移动端三百毫秒，使用fastClick
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
