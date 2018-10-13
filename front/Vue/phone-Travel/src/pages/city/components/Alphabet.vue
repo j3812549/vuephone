38d673478af08e652b3c0205c182aefae563a3ba
逍遥君 <380012546@qq.com>
2018-10-13 16:12:41 +0800

    [A]引入了better-scroll插件，解决ov：hi后无法拉动整体的方法

---
+++ front/Vue/phone-Travel/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.list
+  display flex
+  flex-direction column
+  justify-content center
+  position absolute
+  width .4rem
+  bottom 0
+  right 0
+  top 0.0rem
+  .item
+    line-height .4rem
+    color $bgColor
+    text-align center
+  
+</style>
