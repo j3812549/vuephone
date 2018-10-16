acc60b24a9775fcf526be567c3e4c89731bd6663
逍遥君 <380012546@qq.com>
2018-10-16 17:55:39 +0800

    [A]学习动态路由分配

--- front/Vue/vue-router/vue-router/src/pages/Home.vue
+++ front/Vue/vue-router/vue-router/src/pages/Home.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h1>hello router</h1>
+    <h2>这里是Home</h2>
+    <p><router-link :userId="user.item1" :to="'/user/' + user.item1">动态路由分配User id 1 </router-link></p>
+    <p><router-link :userId="user.item2" :to="'/user/' + user.item2">动态路由分配User id 2 </router-link></p>
+    <p><router-link :userId="user.item3" :to="'/user/' + user.item3">动态路由分配User id 3 </router-link></p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  data () {
+    return {
+      user: {
+        item1: 1,
+        item2: 2,
+        item3: 3
+      }
+    }
+  }
 }
 </script>
 
--- front/Vue/vue-router/vue-router/src/router/index.js
+++ front/Vue/vue-router/vue-router/src/router/index.js
@@ -4,12 +4,34 @@ import Home from '@/pages/Home'
 
 Vue.use(Router)
 
+const User = {
+  template: '<div>User {{ $route.params.id }}</div>',
+  watch: {
+    // 使用watch监视路由的变化,可以使用 beforeRouteUpdate 导航守卫监视
+    '$route' (to, from) {
+      // 将要跳转的路由——to
+      // 现在所在的路由——from
+      // console.log(to, from)
+    }
+  },
+  //  beforeRouteUpdate 导航守卫
+  beforeRouteUpdate (to, from, next) {
+    // console.log(to)
+    // console.log(from)
+    // console.log(next)
+  }
+}
+
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/user/:id',
+      component: User
     }
   ]
 })
